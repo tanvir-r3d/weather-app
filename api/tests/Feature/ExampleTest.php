1eb56af6c73f6d4eae81f7faeca183d6fff98d5b
tanvir <tanvir@skylarksoft.com>
2023-03-06 10:40:44 +0600

    test fixed

--- api/tests/Feature/ExampleTest.php
+++ api/tests/Feature/ExampleTest.php
@@ -17,7 +17,7 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response()
     {
-        $response = $this->get('/');
+        $response = $this->get('/api/v1/users');
 
         $response->assertStatus(200);
     }
